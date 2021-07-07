5f5d77107f3dccb3434d32030747b064218939b3
AustinJoo97 <austinjoo97@gmail.com>
2021-07-07 16:49:28 -0400

    added sixth project to portfolio

--- react-app/src/components/projects.js
+++ react-app/src/components/projects.js
@@ -11,54 +11,57 @@ export default function Projects() {
                 </div>
             </a>
 
-            <a href="https://wait-for-weight-loss.herokuapp.com/" target="_blank" rel="noreferrer">
+            <a href="https://austins-first-tech-blog.herokuapp.com/" target="_blank" rel="noreferrer">
                 <div class="projects" id="project2">
+                    <section class="project_info">
+                        <h4>Tech Blog</h4>
+                        <p>JS, NodeJS, ExpressJS, HandlebarsJS, MySQL</p>
+                    </section>
+                    <a class='ghLink' id='ghLink2' href='https://github.com/AustinJoo97/Tech-Blog' target="_blank" rel="noreferrer">GitHub</a>
+                </div>
+            </a>
+
+            <a href="https://wait-for-weight-loss.herokuapp.com/" target="_blank" rel="noreferrer">
+                <div class="projects" id="project3">
                     <section class="project_info">
                         <h4>Wait for Weight Loss</h4>
                         <p>Handlebars.js, Express, Sequelize, MySQL</p>
                     </section>
-                    <a class='ghLink' id='ghLink2' href='https://github.com/AustinJoo97/WaitForWeightLoss' target="_blank" rel="noreferrer">GitHub</a>
+                    <a class='ghLink' id='ghLink3' href='https://github.com/AustinJoo97/WaitForWeightLoss' target="_blank" rel="noreferrer">GitHub</a>
                 </div>
             </a>
 
             <a href="https://austinjoo97.github.io/weatherReport-gwuhw6/" target="_blank" rel="noreferrer">
-                <div class="projects" id="project3">
+                <div class="projects" id="project4">
                     <section class="project_info projLink">
                         <h4>Weather Report</h4>
                         <p>Javascript/OpenWeatherAPI/Boostrap/HTML/CSS</p>
                     </section>
-                    <a class='ghLink' id='ghLink3' href='https://github.com/AustinJoo97/weatherReport-gwuhw6' target="_blank" rel="noreferrer">GitHub</a>
+                    <a class='ghLink' id='ghLink4' href='https://github.com/AustinJoo97/weatherReport-gwuhw6' target="_blank" rel="noreferrer">GitHub</a>
                 </div>
             </a>
 
             <a href="https://austins-simple-note-taker.herokuapp.com/" target="_blank" rel="noreferrer">
-                <div class="projects" id="project4">
+                <div class="projects" id="project5">
                     <section class="project_info">
                         <h4>Express Note Taker</h4>
                         <p>Javascript/Express/Node.js/HTML/CSS</p>
                     </section>
-                    <a class='ghLink' id='ghLink4' href='https://github.com/AustinJoo97/Note-Taker' target="_blank" rel="noreferrer">GitHub</a>
+                    <a class='ghLink' id='ghLink5' href='https://github.com/AustinJoo97/Note-Taker' target="_blank" rel="noreferrer">GitHub</a>
                 </div>
             </a>
             
             <a href="https://austinjoo97.github.io/Work-Day-Scheduler-gwuhw5/" target="_blank" rel="noreferrer">
-                <div class="projects" id="project5">
+                <div class="projects" id="project6">
                     <section class="project_info projLink">
                         <h4>Work Day Planner</h4>
                         <p>Javascript/jQuery/MomentJS/Bootstrap/HTML</p>
                     </section>
-                    <a class='ghLink' id='ghLink5' href='https://github.com/AustinJoo97/Work-Day-Scheduler-gwuhw5' target="_blank" rel="noreferrer">GitHub</a>
+                    <a class='ghLink' id='ghLink6' href='https://github.com/AustinJoo97/Work-Day-Scheduler-gwuhw5' target="_blank" rel="noreferrer">GitHub</a>
                 </div>
             </a>
 
-            <a href="https://reddit.com" target="_blank" rel="noreferrer">
-                <div class="projects" id="project6">
-                    <section class="project_info">
-                        <h4>Project Title 6</h4>
-                        <p>Technologies used</p>
-                    </section>
-                </div>
-            </a>
+            
         </div>
     )
 }
