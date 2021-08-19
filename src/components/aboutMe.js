b80cb90bb0ea88bb013e7ab5fac622cf0175428c
AustinJoo97 <austinjoo97@gmail.com>
2021-08-19 04:20:33 -0400

    updated aboutMe page with more updated information and made spelling/grammar errors

--- src/components/aboutMe.js
+++ src/components/aboutMe.js
@@ -2,15 +2,18 @@ export default function AboutMe(){
     return (
         <div className="peronsalInfo" id="aboutMe">
             <p>
-                Hello! My name is Austin Joo. I am a software engineer and am currently studying javascript-focused, full-stack software development. I completed my undergraduate program of studies at Rutgers University, The State University of New Jersey. I have experience with the following: 
+                Hello! My name is Austin Joo. I am a recent software engineering graduate with intermediate-level knowledge of software design, development, and testing. I am always seeking to broaden my educational background with excellent analytical, problem-solving, technical, and programming skills. Additionally, I possess other soft skills including being bilingual in my ability to speak Korean, collaborative experience, multitasking, and effective time management.
+                <br></br>
+                <br></br>
+                As a Full-Stack engineer, my technical skills are centered primarily around but not limited to the following: 
             </p>
             <div id="skillset">
                 <ol>
                     <li className="skillList">Dynamic, stateful and stateless frontend user interface development using <strong>React, Handlebars.js, Bootstrap, Foundation, HTML5, and CSS3</strong></li>
-                    <li className="skillList">Caching and authentication using <strong>Bcrypt, Cookies, Session stores, and JSON web tokens</strong></li>
-                    <li className="skillList">Server development, maintenance, and multi-software communications through the use of <strong>Vanilla Javascript, jQuery, ExpressJS, NodeJS, NPM, AJAX, and custom APIs</strong></li>
-                    <li className="skillList">Database design, creation, population, and management through both <strong>Mongo</strong> and <strong>mySQL</strong> via queries compiled in JS using <strong>Mongoose, Sequelize, and GraphL</strong></li>
-                    <li className="skillList">Application deployment and monitoring through <strong>Heroku, GitHub Pages,</strong> and <strong>AWS</strong></li>
+                    <li className="skillList">Caching and authentication using <strong>Bcrypt, Cookies, Session stores, IndexedDBs, localstorage, and JSON Web Tokens</strong></li>
+                    <li className="skillList">Server development, maintenance, and data-centeric, multi-software communications through the use of <strong>JavaScript (ES6+), jQuery, Express.js, Node.js, npm, Axios, AJAX, and both custom and external RESTful APIs</strong></li>
+                    <li className="skillList">Database design, creation, population, and management through both <strong>Mongo</strong> and <strong>MySQL</strong> via queries compiled in JS using <strong>Mongoose, Sequelize, and GraphQL</strong></li>
+                    <li className="skillList">Application version control, deployment and monitoring through <strong>git, Heroku, GitHub Pages, MongoDB Atlas, JawsDB_MySQL, </strong> and <strong>AWS</strong></li>
                 </ol>
             </div>
             <p>I enjoy spending my free time with family and friends, playing both video and board games, exercising, practicing, teaching, and competing in the sport of TaeKwonDo, eating food, and expericing new things, some of which include skydiving, go-cart racing, and eating Korean pufferish stew.</p>
